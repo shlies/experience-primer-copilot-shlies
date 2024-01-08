d82f618f603e95f177be306a901cb0e85dd2e0bb
shlies <61170000+shlies@users.noreply.github.com>
2024-01-08 07:26:31 +0000

    fixbug

--- test.c
+++ test.c
@@ -1,34 +1,26 @@
-
-/*给定n个不同的整数数字（1~9，从小到大依次输入），请编程求出其所能组成的所有n位整数，并按从小到大的顺序依次输出，每行一个整数。要求用递归实现。
-
-如，先输入3，再依次从小到大输入1，2，3三个整数数字，其所有可能组成的3位整数，从小到大依次输出为：
-
-123
-
-132
-
-213
-
-231
-
-312
-
-321
-
-*/
-
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void permute(char *a, int l, int r)
-{
+char *arr[1000];
+int index1 = 0;
+
+void swap(char *x, char *y) {
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(char *a, int l, int r) {
    int i;
-   if (l == r)
-     printf("%s\n", a);
-   else
-   {
-       for (i = l; i <= r; i++)
-       {
+   if (l == r) {
+     arr[index1] = (char*)malloc(sizeof(char)*(r+2));
+     strcpy(arr[index1], a);
+     index1++;
+   }
+   else {
+       for (i = l; i <= r; i++) {
           swap((a+l), (a+i));
           permute(a, l+1, r);
           swap((a+l), (a+i)); // backtrack
@@ -36,18 +28,17 @@ void permute(char *a, int l, int r)
    }
 }
 
-void swap(char *x, char *y)
-{
-    char temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
+int compare(const void *a, const void *b) {
+    return strcmp(*(const char**)a, *(const char**)b);
 }
 
-int main()
-{
-    char str[] = "12345";
+int main() {
+    char str[] = "4567";
     int n = strlen(str);
     permute(str, 0, n-1);
+    qsort(arr, index1, sizeof(const char*), compare);
+    for(int i = 0; i < index1; i++) {
+        printf("%s\n", arr[i]);
+    }
     return 0;
 }
